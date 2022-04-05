c49513a456a8ba6f38979663d230a20a702dc7a5
Jerrypoi <jerry.poi.sz@gmail.com>
2022-04-04 20:09:34 -0500

    arrow button to select the date

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,14 +5,13 @@ import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import {LocalizationProvider} from "@mui/lab";
 import {Box, Grid, Stack, TextField} from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export const Main = () => {
-    const [date, setDate] = React.useState(new Date('2022-01-01'));
+    let [date, setDate] = React.useState(new Date('2022-01-01'));
     // const date = "2022-01-30"
-    const selectedDay = (val) => {
-        console.log(val.toISOString().slice(0, 10))
-    };
-
+    console.log(date)
     return (
         <div>
 
@@ -22,18 +21,33 @@ export const Main = () => {
                          data={data.filter((data) => data.date === date.toISOString().slice(0, 10))}/>
                 </Grid>
 
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker
-                        label="For desktop"
-                        value={date}
-                        minDate={new Date('2017-01-01')}
-                        onChange={(newValue) => {
-                            setDate(newValue);
-                        }}
-                        date={date}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </LocalizationProvider>
+
+                <Stack direction="row" spacing={2} justifyItems={"center"} justifyContent={"center"}
+                       alignItems={"center"}>
+                    <ArrowBackIcon fontSize={"large"} onClick={() => {
+                        let temp = new Date(date)
+                        temp.setDate(date.getDate() - 1)
+                        setDate(temp)
+                    }}/>
+
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                            label="Choose date to display"
+                            value={date}
+                            minDate={new Date('2017-01-01')}
+                            onChange={(newValue) => {
+                                setDate(newValue);
+                            }}
+                            // date={date}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                    <ArrowForwardIcon fontSize={"large"} onClick={() => {
+                        let temp = new Date(date)
+                        temp.setDate(date.getDate() + 1)
+                        setDate(temp)
+                    }}/>
+                </Stack>
             </Stack>
 
         </div>
